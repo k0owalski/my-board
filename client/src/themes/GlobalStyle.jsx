a6881deec0a06abe99d3975001b44d2ca08b7952
kowalski <knrd.kowalik@gmail.com>
2022-10-20 14:13:54 +0200

    Prepared font subsets for the app

--- client/src/themes/GlobalStyle.jsx
+++ client/src/themes/GlobalStyle.jsx
@@ -1,6 +1,26 @@
 import { createGlobalStyle } from 'styled-components';
 
+import montserratLight from 'assets/fonts/Montserrat-Light.woff2';
+import montserratRegular from 'assets/fonts/Montserrat-Regular.woff2';
+import montserratMedium from 'assets/fonts/Montserrat-Medium.woff2';
+import montserratSemiBold from 'assets/fonts/Montserrat-SemiBold.woff2';
+
+import interLight from 'assets/fonts/Inter-Light.woff2';
+import interRegular from 'assets/fonts/Inter-Regular.woff2';
+import interMedium from 'assets/fonts/Inter-Medium.woff2';
+import interSemiBold from 'assets/fonts/Inter-SemiBold.woff2';
+
 const GlobalStyle = createGlobalStyle`
+  @font-face { font-family: 'Montserrat'; src: url('${montserratLight}') format('woff2'); font-weight: 300; }
+  @font-face { font-family: 'Montserrat'; src: url('${montserratRegular}') format('woff2'); font-weight: 400; }
+  @font-face { font-family: 'Montserrat'; src: url('${montserratMedium}') format('woff2'); font-weight: 500; }
+  @font-face { font-family: 'Montserrat'; src: url('${montserratSemiBold}') format('woff2'); font-weight: 600; }
+
+  @font-face { font-family: 'Inter'; src: url('${interLight}') format('woff2'); font-weight: 300; }
+  @font-face { font-family: 'Inter'; src: url('${interRegular}') format('woff2'); font-weight: 400; }
+  @font-face { font-family: 'Inter'; src: url('${interMedium}') format('woff2'); font-weight: 500; }
+  @font-face { font-family: 'Inter'; src: url('${interSemiBold}') format('woff2'); font-weight: 600; }
+
   *, *::before, *::after {
     margin: 0;
     padding: 0;
